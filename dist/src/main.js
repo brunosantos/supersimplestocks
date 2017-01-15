726bead6a59f220cc6ea5c90408dd457ba22efb8
bruno.santos <bruno.santos@gmail.com>
2017-01-15 14:20:28 +0000

    added jasmine

---
+++ dist/src/main.js
@@ -0,0 +1,3 @@
+"use strict";
+var testModule_1 = require("./testModule");
+console.log(testModule_1.test("23"));
---
+++ test/spec/testSpec.ts
@@ -0,0 +1,8 @@
+/// <reference path="../../typings/index.d.ts" />
+
+describe("A suite", function() {
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+});
+
