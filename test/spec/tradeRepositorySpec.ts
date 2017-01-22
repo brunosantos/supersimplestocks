ff1a0b7fa6ba1a99427b614117889fe458367da7
bruno.santos <bruno.santos@gmail.com>
2017-01-22 16:41:21 +0000

    refactor

--- test/spec/tradeRepositorySpec.ts
+++ test/spec/tradeRepositorySpec.ts
@@ -12,44 +12,28 @@ describe("Given a Stock symbol", function() {
                 let price = 156;
                 describe("and tradeType of Buy", function() {
                     let tradeType = Domain.TradeType.Buy;
-                    it("When I Save a Trade", function() {     
-                        let trade = new Domain.Trade(stockSymbol,timeStamp,quantity,tradeType,price);     
-                        let tradeRepo = new Data.TradeRepository();
-                        tradeRepo.Save(trade);
-                        
-                        expect(tradeRepo.Contains(trade)).toBe(true);
-                    });
-                });
-
-                describe("and tradeType of Sell", function() {
-                    let tradeType=Domain.TradeType.Sell;
-                    it("When I Save a Trade", function() {  
-                        let trade = new Domain.Trade(stockSymbol,timeStamp,quantity,tradeType,price);          
-                        let tradeRepo = new Data.TradeRepository();
-                        tradeRepo.Save(trade);
-                        
-                        expect(tradeRepo.Contains(trade)).toBe(true);
+                    describe("When I Save a Trade", function() {    
+                        let trade = new Domain.Trade(stockSymbol,timeStamp,quantity,tradeType,price);   
+                        it("it should store that Trade", function() { 
+                            let tradeRepo = new Data.TradeRepository();
+                            tradeRepo.Save(trade);
+                            
+                            expect(tradeRepo.Contains(trade)).toBe(true);
+                        });
+                        describe("And then Save another Trade", function() {    
+                            let trade2 = new Domain.Trade("GIN",new Date(2017, 1, 15, 9, 24, 0),10,Domain.TradeType.Sell,342);   
+                            it("it should store both Trades", function() { 
+                                let tradeRepo = new Data.TradeRepository();
+                                tradeRepo.Save(trade);
+                                tradeRepo.Save(trade2);
+                                
+                                expect(tradeRepo.Contains(trade)).toBe(true);
+                                expect(tradeRepo.Contains(trade2)).toBe(true);
+                            });
+                        });
                     });
                 });
             });
         });
-        // describe("and quantity of shares of -1", function() {
-        //     let quantity=-1;
-        //     it("When I calculate the P/E Ratio", function() {            
-        //         let dividendYieldCalculator = new Services.PERatioCalculator();
-        //         expect(dividendYieldCalculator.run(simpleStock,marketPrice)).toBe(12.5);
-        //     });            
-        // });
-        // describe("and quantity of shares of 0", function() {
-        //     let quantity=-1;
-        //     it("When I calculate the P/E Ratio", function() {            
-        //         let dividendYieldCalculator = new Services.PERatioCalculator();
-        //         expect(dividendYieldCalculator.run(simpleStock,marketPrice)).toBe(12.5);
-        //     });            
-        // });
     });
-    // describe("and a timestamp in the future", function() {
-    //     let timeStamp = Date.now();
-        
-    // });
 });
